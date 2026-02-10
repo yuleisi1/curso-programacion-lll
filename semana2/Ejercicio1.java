055655b18730b4c45d65aa8bf6cc199b609770c1
yuleisi1 <yscifuentes@estudiante.uniajc.edu.co>
2026-02-10 16:40:32 +0000

    guardar cambio

---
+++ semana2/Ejercicio1.java
@@ -0,0 +1,13 @@
+package semana2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+         // forma1-> definiendo directamente los valores del arreglo
+        int[] a = { 4, 8, 9, 6, 1, 2 };
+
+        //recorrer los elementos del arreglo 
+        for (int i = 0 ; i < a.length ; i++){
+            System.out.println ("a["+i+ "] = " + a[i]);
+}
+    }
+    }
