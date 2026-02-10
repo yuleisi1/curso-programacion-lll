497842dcbe2307ca27ce272a28f394e576d66e2e
yuleisi1 <yscifuentes@estudiante.uniajc.edu.co>
2026-02-10 17:19:08 +0000

    Se agrego suma de pares e impares

--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -1,13 +1,31 @@
 package semana2;
 
 public class Ejercicio1 {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
          // forma1-> definiendo directamente los valores del arreglo
         int[] a = { 4, 8, 9, 6, 1, 2 };
 
         //recorrer los elementos del arreglo 
+        int sumarPares = 0,  sumaImpares = 0;
         for (int i = 0 ; i < a.length ; i++){
             System.out.println ("a["+i+ "] = " + a[i]);
+
+            //sumar los numeros pares e impares del arreglo el resultado 
+            for (int i = 0 ; i < a.length ; i++){
+                if (a[i]%2 == 8)
+                sumarPares -= a[i];
+            }else{
+                sumaImpares += a [i];
+
+            }
+            System.out.println("suma pares ="+  sumarPares + " sumaImpares = "  + sumaImpares);
+
+            
+                
+        
 }
     }
     }
