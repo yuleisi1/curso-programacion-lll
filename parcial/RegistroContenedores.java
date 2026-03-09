df48f662c0a696929f6c84b2074d955f6d008379
yuleisi1 <yscifuentes@estudiante.uniajc.edu.co>
2026-03-09 21:04:55 +0000

    Registro contenedores guardar

---
+++ parcial/RegistroContenedores.java
@@ -0,0 +1,17 @@
+public class RegistroContenedores {
+
+    private String origen;
+    private double peso;
+
+    public RegistroContenedores (String origen, double peso){
+        this.origen = origen;
+        this.peso = peso;
+    }
+    public String getOrigen(){
+        return origen;
+
+    }
+    public double peso(){
+        return peso;
+    }
+}
