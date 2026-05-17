5c27786da05e76ad341d08e22354a976207c820c
yuleisi1 <yscifuentes@estudiante.uniajc.edu.co>
2026-05-17 00:04:32 +0000

    guardar operacion

---
+++ proyecto/Operacion.java
@@ -0,0 +1,13 @@
+package proyecto;
+
+public class Operacion {
+     private String accion;
+
+    public Operacion(String accion) {
+        this.accion = accion;
+    }
+    public String getAccion() {
+        return accion;
+    }
+}
+
